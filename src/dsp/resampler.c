ee37e424646c8d963b87b0e8374d0b424e9dea70
Daniel Forde <daniel.forde001@gmail.com>
2021-12-01 18:01:07 +0200

    Be smarter about when upsampling/downsampling is actually necessary in the resampler.

--- src/dsp/resampler.c
+++ src/dsp/resampler.c
@@ -1,5 +1,7 @@
 #include "resampler.h"
 
+#include <complex.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,7 +12,12 @@ void init_resampler_r(resampler_r_t* resampler, uint32_t interpolation_factor, u
 {
     resampler->interpolation_factor = interpolation_factor;
     resampler->decimation_factor = decimation_factor;
-    resampler->historic_samples = (float*)malloc((decimation_factor - 1) * sizeof(float));
+    if(decimation_factor > 1) {
+        resampler->historic_samples = (float*)malloc((decimation_factor - 1) * sizeof(float));
+    }
+    else {
+        resampler->historic_samples = NULL;
+    }
     resampler->num_historic_samples = 0;
     init_filter_r(&resampler->filter, taps, num_taps);
 }
@@ -18,40 +25,57 @@ void init_resampler_r(resampler_r_t* resampler, uint32_t interpolation_factor, u
 void apply_resampler_r(resampler_r_t* resampler, const real_data_t* const input, real_data_t* const output)
 {
     const uint32_t interpolation_factor = resampler->interpolation_factor;
+    const bool upsampling_reqiured = interpolation_factor > 1;
 
-    real_data_t upsampled_data;
-    upsampled_data.sample_rate_Hz = input->sample_rate_Hz * interpolation_factor;
-    upsampled_data.num_samples = input->num_samples * interpolation_factor;
-    if(interpolation_factor > 1) {
-        upsampled_data.samples = (float*)malloc(upsampled_data.num_samples * sizeof(float));
-        memset(upsampled_data.samples, 0, upsampled_data.num_samples * sizeof(float));
-        for(size_t i = 0; i < upsampled_data.num_samples; i += interpolation_factor) {
-            upsampled_data.samples[i] = input->samples[i / interpolation_factor];
+    float* stage_1_data = NULL;
+    size_t stage_1_num_samples = 0;
+
+    if(upsampling_reqiured) {
+        stage_1_num_samples = input->num_samples * interpolation_factor;
+        stage_1_data = (float*)malloc(stage_1_num_samples * sizeof(float));
+        memset(stage_1_data, 0, stage_1_num_samples * sizeof(float));
+        for(size_t i = 0; i < stage_1_num_samples; i += interpolation_factor) {
+            stage_1_data[i] = input->samples[i / interpolation_factor];
         }
     }
     else {
-        upsampled_data.samples = input->samples;
+        stage_1_data = input->samples;
+        stage_1_num_samples = input->num_samples;
     }
 
     const uint32_t decimation_factor = resampler->decimation_factor;
-    const size_t num_hist_samples = resampler->num_historic_samples;
-    float* hist = resampler->historic_samples;
+    const bool downsampling_required = decimation_factor > 1;
 
-    const size_t total_num_samples = (num_hist_samples + upsampled_data.num_samples) / decimation_factor * decimation_factor;
-    const size_t samples_to_carry_over = (num_hist_samples + upsampled_data.num_samples) % decimation_factor;
-    float* accumulated_data = (float*)malloc(total_num_samples * sizeof(float));
-    memcpy(accumulated_data, hist, num_hist_samples * sizeof(float));
-    memcpy(accumulated_data + num_hist_samples, upsampled_data.samples, (total_num_samples - num_hist_samples) * sizeof(float));
-    memcpy(hist, &upsampled_data.samples[upsampled_data.num_samples - samples_to_carry_over], samples_to_carry_over * sizeof(float));
-    resampler->num_historic_samples = samples_to_carry_over;
+    float* stage_2_data = NULL;
+    size_t stage_2_num_samples = 0;
 
-    destroy_real_data(&upsampled_data);
+    if(downsampling_required) {
+        const size_t num_hist_samples = resampler->num_historic_samples;
+        float* hist = resampler->historic_samples;
+
+        stage_2_num_samples = (num_hist_samples + stage_1_num_samples) / decimation_factor * decimation_factor;
+        const size_t samples_to_carry_over = (num_hist_samples + stage_1_num_samples) % decimation_factor;
+        stage_2_data = (float*)malloc(stage_2_num_samples * sizeof(float));
+        memcpy(stage_2_data, hist, num_hist_samples * sizeof(float));
+        memcpy(stage_2_data + num_hist_samples, stage_1_data, (stage_2_num_samples - num_hist_samples) * sizeof(float));
+        memcpy(hist, &stage_1_data[stage_1_num_samples - samples_to_carry_over], samples_to_carry_over * sizeof(float));
+        resampler->num_historic_samples = samples_to_carry_over;
+    }
+    else {
+        stage_2_data = stage_1_data;
+        stage_2_num_samples = stage_1_num_samples;
+    }
 
     output->sample_rate_Hz = input->sample_rate_Hz * interpolation_factor / decimation_factor;
     fir_filter_r_t filter = resampler->filter;
-    apply_filter_r(&filter, decimation_factor, accumulated_data, total_num_samples, &output->samples, &output->num_samples);
+    apply_filter_r(&filter, decimation_factor, stage_2_data, stage_2_num_samples, &output->samples, &output->num_samples);
 
-    free(accumulated_data);
+    if(downsampling_required) {
+        free(stage_2_data);
+    }
+    if(upsampling_reqiured) {
+        free(stage_1_data);
+    }
 }
 
 void destroy_resampler_r(resampler_r_t* resampler)
@@ -59,7 +83,9 @@ void destroy_resampler_r(resampler_r_t* resampler)
     destroy_filter_r(&resampler->filter);
     resampler->interpolation_factor = 1;
     resampler->decimation_factor = 1;
-    free(resampler->historic_samples);
+    if(resampler->historic_samples != NULL) {
+        free(resampler->historic_samples);
+    }
     resampler->num_historic_samples = 0;
 }
 
@@ -75,40 +101,57 @@ void init_resampler_c(resampler_c_t* resampler, uint32_t interpolation_factor, u
 void apply_resampler_c(resampler_c_t* resampler, const iq_data_t* const input, iq_data_t* const output)
 {
     const uint32_t interpolation_factor = resampler->interpolation_factor;
+    const bool upsampling_reqiured = interpolation_factor > 1;
 
-    iq_data_t upsampled_data;
-    upsampled_data.sample_rate_Hz = input->sample_rate_Hz * interpolation_factor;
-    upsampled_data.num_samples = input->num_samples * interpolation_factor;
-    if(interpolation_factor > 1) {
-        upsampled_data.samples = (float complex*)malloc(upsampled_data.num_samples * sizeof(float complex));
-        memset(upsampled_data.samples, 0, upsampled_data.num_samples * sizeof(float complex));
-        for(size_t i = 0; i < upsampled_data.num_samples; i += interpolation_factor) {
-            upsampled_data.samples[i] = input->samples[i / interpolation_factor];
+    float complex* stage_1_data = NULL;
+    size_t stage_1_num_samples = 0;
+
+    if(upsampling_reqiured) {
+        stage_1_num_samples = input->num_samples * interpolation_factor;
+        stage_1_data = (float complex*)malloc(stage_1_num_samples * sizeof(float complex));
+        memset(stage_1_data, 0, stage_1_num_samples * sizeof(float complex));
+        for(size_t i = 0; i < stage_1_num_samples; i += interpolation_factor) {
+            stage_1_data[i] = input->samples[i / interpolation_factor];
         }
     }
     else {
-        upsampled_data.samples = input->samples;
+        stage_1_data = input->samples;
+        stage_1_num_samples = input->num_samples;
     }
 
     const uint32_t decimation_factor = resampler->decimation_factor;
-    const size_t num_hist_samples = resampler->num_historic_samples;
-    float complex* hist = resampler->historic_samples;
+    const bool downsampling_required = decimation_factor > 1;
 
-    const size_t total_num_samples = (num_hist_samples + upsampled_data.num_samples) / decimation_factor * decimation_factor;
-    const size_t samples_to_carry_over = (num_hist_samples + upsampled_data.num_samples) % decimation_factor;
-    float complex* accumulated_data = (float complex*)malloc(total_num_samples * sizeof(float complex));
-    memcpy(accumulated_data, hist, num_hist_samples * sizeof(float complex));
-    memcpy(accumulated_data + num_hist_samples, upsampled_data.samples, (total_num_samples - num_hist_samples) * sizeof(float complex));
-    memcpy(hist, &upsampled_data.samples[upsampled_data.num_samples - samples_to_carry_over], samples_to_carry_over * sizeof(float complex));
-    resampler->num_historic_samples = samples_to_carry_over;
+    float complex* stage_2_data = NULL;
+    size_t stage_2_num_samples = 0;
 
-    destroy_iq_data(&upsampled_data);
+    if(downsampling_required) {
+        const size_t num_hist_samples = resampler->num_historic_samples;
+        float complex* hist = resampler->historic_samples;
+
+        stage_2_num_samples = (num_hist_samples + stage_1_num_samples) / decimation_factor * decimation_factor;
+        const size_t samples_to_carry_over = (num_hist_samples + stage_1_num_samples) % decimation_factor;
+        stage_2_data = (float complex*)malloc(stage_2_num_samples * sizeof(float complex));
+        memcpy(stage_2_data, hist, num_hist_samples * sizeof(float complex));
+        memcpy(stage_2_data + num_hist_samples, stage_1_data, (stage_2_num_samples - num_hist_samples) * sizeof(float complex));
+        memcpy(hist, &stage_1_data[stage_1_num_samples - samples_to_carry_over], samples_to_carry_over * sizeof(float complex));
+        resampler->num_historic_samples = samples_to_carry_over;
+    }
+    else {
+        stage_2_data = stage_1_data;
+        stage_2_num_samples = stage_1_num_samples;
+    }
 
     output->sample_rate_Hz = input->sample_rate_Hz * interpolation_factor / decimation_factor;
     fir_filter_c_t filter = resampler->filter;
-    apply_filter_c(&filter, decimation_factor, accumulated_data, total_num_samples, &output->samples, &output->num_samples);
+    apply_filter_c(&filter, decimation_factor, stage_2_data, stage_2_num_samples, &output->samples, &output->num_samples);
 
-    free(accumulated_data);
+    if(downsampling_required) {
+        free(stage_2_data);
+    }
+    if(upsampling_reqiured) {
+        free(stage_1_data);
+    }
 }
 
 void destroy_resampler_c(resampler_c_t* resampler)
